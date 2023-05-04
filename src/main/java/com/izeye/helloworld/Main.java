4369a91da24683ecbc5307db589300306c46589e
Johnny Lim <izeye@naver.com>
2023-05-04 20:43:47 +0900

    Set appendLineSeparator

--- src/main/java/com/izeye/helloworld/Main.java
+++ src/main/java/com/izeye/helloworld/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger("jsonLogger");
         logger.debug("Hello,\nworld!");
+        logger.debug("Hello,\nworld!");
     }
 
 }
